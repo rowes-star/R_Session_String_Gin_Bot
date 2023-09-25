456056bb821665d41c6119702d33fe45b87a84a5
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2023-09-25 13:21:43 +0300

    Add files via upload

---
+++ StringGenBot/must_join.py
@@ -0,0 +1,30 @@
+from env import MUST_JOIN
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+
+
+@Client.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: Client, msg: Message):
+    if not MUST_JOIN:  # Not compulsory
+        return
+    try:
+        try:
+            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await bot.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply_photo(photo="https://telegra.ph/file/654ddaf472f18b799600b.jpg", caption=f"↯︙يحݪـو عـݪـيك اެݪاެشـتراެك فـي قـنـاެة اެݪـبـوت 📻 .\n↯︙يوزر القناة ( @{link} ) .",
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("-", url=f"{link}")]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"Promote me as an admin in the MUST_JOIN chat : {MUST_JOIN} !")
