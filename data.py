5057a21f3ca85878762992d04adb23e9d1ef4305
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2024-05-12 16:41:09 +0300

    Update data.py

--- data.py
+++ data.py
@@ -2,18 +2,18 @@ from pyrogram.types import InlineKeyboardButton
 
 
 class Data:
-    generate_single_button = [InlineKeyboardButton("↯︙ انشاء كود بايروجرام .", callback_data="generate")]
+    generate_single_button = [InlineKeyboardButton("↯︙ انشاء جلسة سيشن  .", callback_data="generate")]
 
     generate_button = [generate_single_button]
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("↯︙المطور .", url="https://t.me/RO_V0"),
+        [InlineKeyboardButton("↯︙المطور .", url="https://t.me/R7_OX"),
         ],
     ]
 
     START = """
-↯︙اهلا بك في بوت انشاء كود بايروجرام .
+↯︙اهلا بك في بوت انشاء كود جلسة بايروجرام ، تليثون .
 
 ↯︙ يمكنك انشاء كود بايروجرام بسهولة .
 
